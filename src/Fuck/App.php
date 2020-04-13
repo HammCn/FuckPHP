e5884076ac047871091b463def33f8c627508cd8
Hamm <majhamm@qq.com>
2020-04-13 17:39:00 +0800

    init

---
+++ src/Fuck/App.php
@@ -0,0 +1,12 @@
+<?php
+namespace Fuck;
+class App{
+    const VERSION = '1.0.0';
+    protected $debug = false;
+    public function __construct(){
+
+    }
+    public function run(){
+        return $this;
+    }
+}
